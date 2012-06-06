28e140f0d5bb88c9612dff1e339d39d5d871a6ce
saasbook <saasbook@saasbook.(none)>
2012-06-06 03:26:57 -0700

    added css, movie view and controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,9 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.html.haml by default
+  end
 end
